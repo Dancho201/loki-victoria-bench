367b4275f002813ac697a5261b286f7d0df23b12
Yordan Basin <yordan.basin@notolytix.com>
2024-06-30 13:07:03 +0300

    feat: ip output

--- provision/main.tf
+++ provision/main.tf
@@ -110,3 +110,14 @@ resource "lxd_instance" "vlogs-bench" {
     }
   }
 }
+
+
+output "vlogs_bench_ip" {
+  value = lxd_instance.vlogs-bench.ipv4_address
+  
+}
+
+output "lok_bench_ip" {
+  value = lxd_instance.loki-bench.ipv4_address
+  
+} 
