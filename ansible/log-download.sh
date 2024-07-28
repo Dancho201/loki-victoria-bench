0f38932293d9191a9a995a5f6a018d4700853ea3
Yordan Basin <yordan.basin@notolytix.com>
2024-07-28 20:40:28 +0300

    [small chagnes]

--- ansible/log-download.sh
+++ ansible/log-download.sh
@@ -7,7 +7,7 @@ mkdir -p "$LOG_DIR"
 
 declare -A files=(
   ["Apache.tar.gz"]="https://zenodo.org/record/3227177/files/Apache.tar.gz?download=1"
-  ["HDFS_1.tar.gz"]="https://zenodo.org/record/3227177/files/HDFS_1.tar.gz?download=1"
+  ["HDFS_1.tar.gz"]="https://zenodo.org/record/3227177/files/HDFS_2.tar.gz?download=1"
   ["Linux.tar.gz"]="https://zenodo.org/record/3227177/files/Linux.tar.gz?download=1"
   ["Thunderbird.tar.gz"]="https://zenodo.org/record/3227177/files/Thunderbird.tar.gz?download=1"
   ["SSH.tar.gz"]="https://zenodo.org/record/3227177/files/SSH.tar.gz?download=1"
